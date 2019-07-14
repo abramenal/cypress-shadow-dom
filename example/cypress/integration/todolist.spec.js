8c19d5d212e31b8b8e2722d1557d4937a6df3674
abramenal <pavel.auramenka@gmail.com>
2019-07-14 23:53:57 +0300

    Update test suite to reflect latest implementation

--- example/cypress/integration/todolist.spec.js
+++ example/cypress/integration/todolist.spec.js
@@ -3,48 +3,63 @@ describe('Todo List', () => {
     cy.visit('/');
   });
 
-  /*
-   * https://github.com/cypress-io/snapshot
-   * Add manual DOM snapshots
-   */
-
   it('renders list correctly', () => {
-    cy.document({ log: false })
-      .shadowGet('todo-list todo-list-item')
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
       .its('length')
       .should('eq', 2);
   });
 
   it('displays first item title correctly', () => {
-    cy.document({ log: false })
-      .shadowGet('todo-list todo-list-item')
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
       .shadowFirst()
       .shadowContains('Read some books');
 
-    /* Or same thing with other command */
-    cy.document({ log: false })
-      .shadowGet('todo-list')
+    /* Or, same thing via */
+    cy.shadowGet('todo-list')
       .shadowFind('todo-list-item')
+
+      // TODO: test negative values
+      .shadowEq(-2)
       .shadowContains('Read some books');
   });
 
   it('displays last item title correctly', () => {
-    cy.document({ log: false })
-      .shadowGet('todo-list todo-list-item')
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
       .shadowLast()
+      .shadowContains('Buy some serials');
+
+    /* Or, same thing via */
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
+      .shadowEq()
       .shadowContains('Buy some serials');
   });
 
   it('removes list item when clicking X (remove) button', () => {
-    cy.document({ log: false })
-      .shadowGet('todo-list todo-list-item')
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
       .shadowFirst()
       .shadowFind('button.destroy')
       .shadowTrigger('click');
 
-    cy.document({ log: false })
-      .shadowGet('todo-list todo-list-item')
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-list-item')
       .its('length')
       .should('eq', 1);
+  });
+
+  it('displays correct search query value', () => {
+    const QUERY = 'books';
+
+    cy.shadowGet('todo-list')
+      .shadowFind('todo-form')
+      .shadowFind('input')
+      .shadowType(QUERY)
+      .then(([$el]) => {
+        expect($el.value).to.equal(QUERY);
+      });
   });
 });
