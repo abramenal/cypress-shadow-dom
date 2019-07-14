6503df6ad01bf23ab2fa032268348c2b27a223fa
abramenal <pavel.auramenka@gmail.com>
2019-07-14 23:53:31 +0300

    Update commands bootstrapping

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import { shadowClick, shadowContains, shadowFind, shadowFirst, shadowGet, shadowLast, shadowTrigger } from './commands';
+import {
+  shadowClick,
+  shadowContains,
+  shadowEq,
+  shadowFind,
+  shadowFirst,
+  shadowGet,
+  shadowLast,
+  shadowTrigger,
+  shadowType,
+} from './commands';
 
 export default () => {
-  Cypress.Commands.add('shadowClick', { prevSubject: true }, shadowClick);
-  Cypress.Commands.add('shadowGet', { prevSubject: 'document' }, shadowGet);
-  Cypress.Commands.add('shadowContains', { prevSubject: true }, shadowContains);
-  Cypress.Commands.add('shadowTrigger', { prevSubject: true }, shadowTrigger);
-  Cypress.Commands.add('shadowFind', { prevSubject: true }, shadowFind);
-  Cypress.Commands.add('shadowFirst', { prevSubject: true }, shadowFirst);
-  Cypress.Commands.add('shadowLast', { prevSubject: true }, shadowLast);
+  const commandOptions = { prevSubject: true };
+
+  Cypress.Commands.add('shadowClick', commandOptions, shadowClick);
+  Cypress.Commands.add('shadowContains', commandOptions, shadowContains);
+  Cypress.Commands.add('shadowEq', commandOptions, shadowEq);
+  Cypress.Commands.add('shadowFind', commandOptions, shadowFind);
+  Cypress.Commands.add('shadowFirst', commandOptions, shadowFirst);
+  Cypress.Commands.add('shadowGet', { prevSubject: false }, shadowGet);
+  Cypress.Commands.add('shadowLast', commandOptions, shadowLast);
+  Cypress.Commands.add('shadowTrigger', commandOptions, shadowTrigger);
+  Cypress.Commands.add('shadowType', commandOptions, shadowType);
 };
