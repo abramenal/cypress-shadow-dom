7cfd0d3bc0d28911e1a4b7d6882e8886a1fe502d
abramenal <pavel.auramenka@gmail.com>
2019-10-20 18:33:59 +0300

    Fix linting errors

--- src/validators/validateSelector.js
+++ src/validators/validateSelector.js
@@ -10,8 +10,8 @@ export default selector => {
    * e.g. `input[placeholder="First A Name"]` will not be matched
    * however `div input[placeholder="Text"]` will find the whitespace
    */
-  const illegalSpaceBeforeRegex = /(?<!\".+)(\s)/g;
-  const illegalSpaceAfterRegex = /(\s)(?!.+\")/g;
+  const illegalSpaceBeforeRegex = /(?<!".+)(\s)/g;
+  const illegalSpaceAfterRegex = /(\s)(?!.+")/g;
 
   if (!!selector.match(illegalSpaceBeforeRegex) || !!selector.match(illegalSpaceAfterRegex)) {
     throw new InternalError(ERR_TYPES.INVALID_SELECTOR_NO_MULTI);
