815330e1edf6190361f702af701e1e7980229ae1
abramenal <pavel.auramenka@gmail.com>
2019-10-20 18:27:40 +0300

    improve selector whitespace validation to disallow it only for multiple selectors (like tags, etc) but allow for enclosed (like attribute values)

--- src/validators/validateSelector.js
+++ src/validators/validateSelector.js
@@ -5,7 +5,15 @@ export default selector => {
     throw new InternalError(ERR_TYPES.INVALID_SELECTOR);
   }
 
-  if (selector.indexOf(' ') !== -1) {
+  /*
+   * Matches any spaces not inside ""
+   * e.g. `input[placeholder="First A Name"]` will not be matched
+   * however `div input[placeholder="Text"]` will find the whitespace
+   */
+  const illegalSpaceBeforeRegex = /(?<!\".+)(\s)/g;
+  const illegalSpaceAfterRegex = /(\s)(?!.+\")/g;
+
+  if (!!selector.match(illegalSpaceBeforeRegex) || !!selector.match(illegalSpaceAfterRegex)) {
     throw new InternalError(ERR_TYPES.INVALID_SELECTOR_NO_MULTI);
   }
 
