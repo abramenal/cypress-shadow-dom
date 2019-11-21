f44cacf7ce46a332d5376bb621b0b46c776b856c
Lucas Schnüriger <lucas.schnueriger@me.com>
2019-11-21 18:44:56 +0100

    Make command options optional in type description
    Since all EventOptions properties are optional anyways and the options are not required, the type definition should reflect this.

--- index.d.ts
+++ index.d.ts
@@ -18,9 +18,9 @@ declare namespace Cypress {
     shadowClick(options?: EventOptions): Chainable<Subject>;
     shadowContains(content: string): Chainable<Subject>;
     shadowEq(index: number): Chainable<Subject>;
-    shadowFind(selector: string, options: CommandOptions): Chainable<Subject>;
+    shadowFind(selector: string, options?: CommandOptions): Chainable<Subject>;
     shadowFirst(): Chainable<Subject>;
-    shadowGet(selector: string, options: CommandOptions): Chainable<Subject>;
+    shadowGet(selector: string, options?: CommandOptions): Chainable<Subject>;
     shadowLast(): Chainable<Subject>;
     shadowTrigger(eventName: string, eventOptions?: EventOptions): Chainable<Subject>;
     shadowType(content: string): Chainable<Subject>;
